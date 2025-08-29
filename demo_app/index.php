b57a03e332c92d2328c6256d7a09b38e64effc2e
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-08-29 14:17:07 +0530

    initial commit

---
+++ demo_app/index.php
@@ -0,0 +1,10 @@
+<?php
+echo "Hello, World!";
+?>
+
+
+<!-- mklink /J "C:\xampp\htdocs\ride_app" "D:\Projects\ibm-ui\php-demos\ride_app" -->
+
+<!-- ln -s /Volumes/D/Projects/ibm-ui/php-demos/ride_app /Applications/XAMPP/htdocs/ride_app -->
+
+<!-- http://localhost/demo_app/ -->
