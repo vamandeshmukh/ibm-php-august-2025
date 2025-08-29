bc02b4ae3aba3579655d83fead44290d515a73fa
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-08-29 15:10:54 +0530

    initial commit

--- ride_app/index.php
+++ ride_app/index.php
@@ -1,27 +1,32 @@
 <?php
+include "calc.php";
 
-echo "Variable declaration in PHP";
+echo "Variable declaration in PHP <br />";
 
 $id = 101;
 $name = "Sonu Joshi";
 $salary = 10.50;
 $married = false;
 
-echo $id;
-echo $name;
-echo $salary;
+// echo $id;
+// echo $name;
+// echo $salary;
+
+echo "Id: $id, Name: $name, Salary: $salary, Married? $married <br />";
+echo "String concatination is done " . "like this.<br />";
 
 function hi() {
-    echo "Hi! What's up?";
+    echo "Hi! What's up? <br />";
 }
 
 hi();
+
+echo addNums(10, 20);
 
 ?>
 
 <!-- <?php
 echo "Hello, World!";
-
 ?> -->
 
 
@@ -30,3 +35,6 @@ echo "Hello, World!";
 <!-- ln -s /Volumes/D/Projects/ibm-ui/php-demos/ride_app /Applications/XAMPP/htdocs/ride_app -->
 
 <!-- http://localhost/demo_app/ -->
+
+
+<!-- https://jsonplaceholder.typicode.com/ -->
